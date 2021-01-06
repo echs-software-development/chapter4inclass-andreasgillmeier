fef1c89b2feaf49881d4b5bd2ea31f5474c2a632
Andreas Gillmeier <ag1110@students.necsd.net>
2021-01-06 20:36:43 +0000

    I finished

--- Prob3.java
+++ Prob3.java
@@ -2,7 +2,12 @@ import java.util.Scanner;
 
 public class Prob3 {
   public static void main(String[] args) {
-    
+  Scanner scan = new Scanner(System.in);
+  System.out.println("Enter an ASCII code: ");
+  
+  int ascii = scan.nextInt();
+  System.out.println((char)ascii);
+  
 
 
   }
